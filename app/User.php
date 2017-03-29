fceef0b927b9a175c16cddab4e7d2b59369e91a0
Hossam Ahmed <hossam.elfar95@gmail.com>
2017-03-29 16:11:43 +0300

    Adding the relations to user model

--- app/User.php
+++ app/User.php
@@ -15,7 +15,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'email', 'password', 'second_name', 'phone', 'code', 'country', 'city', 'age', 'gender', 'pp',
+        'pp', 'user_level'
     ];
 
     /**
@@ -26,4 +27,64 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function answers()
+    {
+        return $this->hasMany('App\Answer');
+    }
+
+    public function fields()
+    {
+        return $this->hasMany('App\Field');
+    }
+
+    public function grades()
+    {
+        return $this->hasMany('App\Grade');
+    }
+
+    public function links()
+    {
+        return $this->hasMany('App\Link');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany('App\Message');
+    }
+
+    public function news()
+    {
+        return $this->hasMany('App\News');
+    }
+
+    public function pictures()
+    {
+        return $this->hasMany('App\Picture');
+    }
+
+    public function programs()
+    {
+        return $this->hasMany('App\Program');
+    }
+
+    public function qs()
+    {
+        return $this->hasMany('App\Q');
+    }
+
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
+
+    public function questioners()
+    {
+        return $this->hasMany('App\Questionnaire');
+    }
+
+    public function videos()
+    {
+        return $this->hasMany('App\Video');
+    }
 }
