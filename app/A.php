5cf3162d3f83b8a3d761360e61a1d52921402c6a
Hossam Ahmed <hossam.elfar95@gmail.com>
2017-03-29 16:14:55 +0300

    Adding the A model with the relation

--- app/A.php
+++ app/A.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class A extends Model
 {
-    //
+    protected $fillable =['answer','verified','question_id','user_id'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
