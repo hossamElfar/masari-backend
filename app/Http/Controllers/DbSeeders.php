347a1632750894c712ac605e306f6e2f74501236
Hossam Ahmed <hossam.elfar95@gmail.com>
2017-04-12 19:07:28 +0300

    seeding data

--- app/Http/Controllers/DbSeeders.php
+++ app/Http/Controllers/DbSeeders.php
@@ -53,7 +53,7 @@ class DbSeeders extends Controller
 
     public function seedDecisionArabic(Request $request)
     {
-        $questionnaire = Questionnaire::find(267);
+        $questionnaire = Questionnaire::find(3);
         $data = $request->all();
         $question1 = new Question([
             'question_content' => $data['data'][0],
